393de76b1948b71760cd614be9bc6e8dd7d7d43d
Nikoli <huseynlinihad4400@gmail.com>
2024-06-19 20:14:01 +0400

    ....

---
+++ SignalRApi/Controllers/SliderController.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SliderController : ControllerBase
+    {
+        private readonly ISliderService _sliderService;
+        private readonly IMapper _mapper;
+
+        public SliderController(ISliderService sliderService, IMapper mapper)
+        {
+            _sliderService = sliderService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SliderList()
+        {
+            return Ok(_sliderService.TGetListAll());
+        }
+    }
+}
