a12577d92da0c51eb8eefad714b52ba35afbfd9e
Nikoli <huseynlinihad4400@gmail.com>
2024-06-19 17:12:08 +0400

    ...

---
+++ SignalRWebUI/Controllers/UILayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.Controllers
+{
+	public class UILayoutController : Controller
+	{
+		public IActionResult UILayoutPage()
+		{
+			return View();
+		}
+	}
+}
---
+++ SignalRWebUI/ViewComponents/UILayoutComponents/_UILayoutHeaderPartialComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.Identity.Client;
+
+namespace SignalRWebUI.ViewComponents.UILayoutComponents
+{
+	public class _UILayoutHeaderPartialComponent : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
