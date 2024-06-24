91cfe0aaf1d23233475acb29dc543d68b3d1103a
Nikoli <huseynlinihad4400@gmail.com>
2024-06-24 11:14:56 +0400

    ...

---
+++ SignalRApi/Controllers/NotificationController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.NotificationDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class NotificationController : ControllerBase
+	{
+		private readonly INotificationService _notificationService;
+
+		public NotificationController(INotificationService notificationService)
+		{
+			_notificationService = notificationService;
+		}
+		[HttpGet]
+		public IActionResult NotificationList()
+		{
+			return Ok(_notificationService.TGetListAll());
+		}
+		[HttpGet("NotificationCountByStatusFalse")]
+		public IActionResult NotificationCountByStatusFalse()
+		{
+			return Ok(_notificationService.TNotificationCountByStatusFalse());	
+		}
+		[HttpGet("GetAllNotificationsByFalse")]
+		public IActionResult GetAllNotificationsByFalse()
+		{
+			return Ok(_notificationService.TGetAllNotificationsByFalse());
+		}
+		[HttpPost]
+		public IActionResult CreateNotification(CreateNotificationDto createNotificationDto)
+		{
+			_notificationService.TAdd(new Notification()
+			{
+				Type=createNotificationDto.Type,
+				Icon=createNotificationDto.Icon,
+				Description=createNotificationDto.Description,
+				Date=createNotificationDto.Date,
+				Status=false
+			});
+			return Ok("Notification eklendi");
+		}
+		[HttpDelete("{id}")]
+		public IActionResult DeleteNotification(int id)
+		{
+			var value = _notificationService.TGetById(id);
+			_notificationService.TDelete(value);
+			return Ok("Notification başarıyla silindi.");
+		}
+		[HttpPut]
+		public IActionResult UpdateNotification(UpdateNotificationDto updateNotificationDto)
+		{
+			_notificationService.TUpdate(new Notification()
+			{
+				NotificationId=updateNotificationDto.NotificationId,
+				Type=updateNotificationDto.Type,
+				Icon=updateNotificationDto.Icon,
+				Description=updateNotificationDto.Description,
+				Date=updateNotificationDto.Date,
+				Status=updateNotificationDto.Status
+			});
+			return Ok("Notification başarıyla güncellendi.");
+		}
+		[HttpGet("{id}")]
+		public IActionResult GetNotification(int id)
+		{
+			var value = _notificationService.TGetById(id);
+			return Ok(value);
+		}
+	}
+}
---
+++ SignalRşDataAccessLayer/EntityFrameWork/EfNotificationDal.cs
@@ -0,0 +1,31 @@
+﻿using SignalR.DataAccessLayer.Abstract;
+using SignalR.DataAccessLayer.Concrete;
+using SignalR.DataAccessLayer.Repositories;
+using SignalR.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DataAccessLayer.EntityFrameWork
+{
+	public class EfNotificationDal : GenericRepository<Notification>, INotificationDal
+	{
+		public EfNotificationDal(SignalRContext context) : base(context)
+		{
+		}
+
+		public List<Notification> GetAllNotificationsByFalse()
+		{
+			using var _context=new SignalRContext();
+			return _context.Notifications.Where(n=>n.Status==false).ToList();	
+		}
+
+		public int NotificationCountByStatusFalse()
+		{
+			using var _context = new SignalRContext();
+			return _context.Notifications.Where(n=>n.Status==false).Count();	
+		}
+	}
+}
